7090df186856e09949b5bb7eae1f072f5434de01
amarbat <amarbat@gmail.com>
2021-02-27 17:28:48 -0500

    fix github link

---
+++ precache-manifest.51040255f03cb137d7dd1f349b2a4803.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5338ffb0a81ba587fcd463e1d0f1bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b1adcbe8e91f8c215a7",
+    "url": "/static/css/main.fd9081d0.chunk.css"
+  },
+  {
+    "revision": "8135f7c29bc026f5eedc",
+    "url": "/static/js/2.0ed19bf3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0ed19bf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1adcbe8e91f8c215a7",
+    "url": "/static/js/main.6133be06.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/static/media/GITHUBLOGO.cefc2023.png"
+  },
+  {
+    "revision": "96eb200259447b3362036783138d4680",
+    "url": "/static/media/JAVA.96eb2002.png"
+  },
+  {
+    "revision": "1c72a7b09dc42dbbe74082233dfba54f",
+    "url": "/static/media/LINKEDINICON.1c72a7b0.png"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/static/media/MONGO.09ed2051.png"
+  },
+  {
+    "revision": "0d8ed365842d251cd2bfa6e98d461d62",
+    "url": "/static/media/NODEJS.0d8ed365.png"
+  },
+  {
+    "revision": "cfe08d2f6feb8a227496f6cf6b66db7d",
+    "url": "/static/media/Pic.cfe08d2f.jpg"
+  },
+  {
+    "revision": "df70b00589269cc7e903d0e3ba9db627",
+    "url": "/static/media/REACT.df70b005.png"
+  },
+  {
+    "revision": "815f40789226a46453943b3fe7ad1eaf",
+    "url": "/static/media/REST.815f4078.png"
+  },
+  {
+    "revision": "218124ddca16a47af056a4a6771d40b5",
+    "url": "/static/media/Web-Developer.218124dd.jpg"
+  }
+]);
